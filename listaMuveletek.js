41baec2d3214313f6b9f71cd441cf31eb9120fdf
bajarigergo <gergob10@gmail.com>
2024-04-25 12:06:35 +0200

    rendezes nem találja az optionok értékét

--- listaMuveletek.js
+++ listaMuveletek.js
@@ -1,8 +1,8 @@
 export function kartyaLetrehoz(LISTA) {
-  let txt="";
-  LISTA.forEach((elem, index ) => {
+  let txt = "";
+  LISTA.forEach((elem, index) => {
     txt += `
-        <div class="m-3 p-0 col-3 card ">
+        <div class="m-3 p-0 col-3 card">
         <img
         class="card-img-top"
         src="${elem.kep}"
@@ -10,7 +10,7 @@ export function kartyaLetrehoz(LISTA) {
         style="width: 100%"
       />
       <div class="card-body d-flex flex-column justify-content-around">        
-        <h4 class="card-title">${elem.nev}</h4>
+        <h4 class="card-title nev">${elem.nev}</h4>
         <p class="card-text">Teljesítmény: ${elem.telj} lőerő</p>
         <p class="card-text">Hajtáslánc: ${elem.hajtas}</p>
         <p class="card-text">Ár: ${elem.ar} Millió FT</p>
@@ -27,6 +27,22 @@ export function megjelenit(txt) {
   const ELEM = $(".termekek");
   ELEM.html(txt);
 }
-export function kosarba(VASAROLT) {
+
+export function kosarba(VASAROLT) {}
+
+export function rendez(lista, kulcs, rIrany) {
+  const RLISTA = lista.sort(function (e1,e2) {
+      //returnbe negatív vagy poz érték kell
+      // a nev szöveges változó, ezért < > értelmezhető
+
+      return e1[kulcs]<e2[kulcs]?-1*rIrany:1*rIrany   
+  })
   
+  return RLISTA
+}
+export function szures(lista, keresoSzoveg) {
+  const szLISTA = lista.filter(function (ember) {
+      return ember.nev.toUpperCase().includes(keresoSzoveg.toUpperCase())
+  })
+  return szLISTA
 }
--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 import { LISTA } from "./adat.js";
-import { kartyaLetrehoz, megjelenit } from "./listaMuveletek.js";
+import { kartyaLetrehoz, megjelenit, rendez } from "./listaMuveletek.js";
 
 
-megjelenit(kartyaLetrehoz(LISTA))
+//megjelenit(kartyaLetrehoz(LISTA))
 
 /* Amikor rákatt a kosárba gombra, 
 akkor létrehozol obj: {index:3, db:1}
 belepush a kosár listába vagy növel
 */
+
+
+
+init(LISTA)
+function init(lista) {
+
+  let szoveg = kartyaLetrehoz(lista);
+  megjelenit(szoveg);
+  rendezEsemeny()
+}
+
+function rendezEsemeny() {
+    const listaOpcioELEM = $(".rendez_select")
+    listaOpcioELEM.on("change", function () {
+        let ertek = listaOpcioELEM.val()
+        console.log(ertek);
+        if (ertek == 4) {
+            const LISTA = rendez(LISTA,"nev", 1)
+            init(LISTA)
+
+        }
+        if (ertek == 3) {
+            const LISTA = rendez(LISTA,"nev", -1)
+            init(LISTA)
+        }
+
+    })
+}
